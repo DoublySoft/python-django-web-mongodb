ff151bb2b5786af017fcbc22ec8dabd771219f56
Daniel Gil <dgilalegre96@gmail.com>
2020-12-08 02:23:21 +0100

    Agregar guardado de dimensiones de un libro

--- book/forms.py
+++ book/forms.py
@@ -34,5 +34,33 @@ class BookForm(forms.ModelForm):
 
     def is_valid(self):
         self.data._mutable = True
-        self.data['category'] = ObjectId(self.data['category'])
-        return super(BookForm, self).is_valid()
+        self.data["category"] = ObjectId(self.data["category"])
+
+        if 'dimension' in self.errors:
+            del self.errors['dimension']
+
+        if len(self.errors) == 0:
+            self.data['dimension'] = {
+                "_id": ObjectId(),
+                "x": self.data['dimension-x'],
+                "y": self.data['dimension-y'],
+                "z": self.data['dimension-z'],
+            }
+
+            del self.data['dimension-x']
+            del self.data['dimension-y']
+            del self.data['dimension-z']
+
+            return True
+
+        # return super(BookForm, self).is_valid()
+        return False
+
+    def save(self, commit=True):
+        instance = super(BookForm, self).save(commit=False)
+        instance.dimension = self.data['dimension']
+
+        if commit:
+            instance.save()
+
+        return instance
--- book/urls.py
+++ book/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 book_patterns = (
     [
-        path('', views.list, name='list'),
-        path('create', views.create, name='create'),
-        path('update/<str:pk>', views.update, name='update'),
-        path('delete/<str:pk>', views.delete, name='delete'),
-        path('update/json-get-book-by-id/<str:pk>', views.json_get_book_by_id, name='json'),
+        path("", views.list, name="list"),
+        path("create", views.create, name="create"),
+        path("update/<str:pk>", views.update, name="update"),
+        path("delete/<str:pk>", views.delete, name="delete"),
+        path("update/json-get-book-by-id/<str:pk>", views.json_get_book_by_id, name="json"),
     ],
-    'book'
+    "book"
 )
